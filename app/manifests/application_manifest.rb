c957078b27fb2bac9241aeb53fc4ce8a5c40001b
Benjamin Stein <ben@mcommons.com>
2011-02-08 19:36:38 -0500

    xxxx

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -3,23 +3,20 @@ class ApplicationManifest < Moonshine::Manifest::Rails
 
   # Rails, Apache, Passenger, the database from database.yml, Postfix, Cron, logrotate and NTP.
   recipe :default_stack
-
-
-
-
-  # recipe :memcached
-  # 
-  # def application_packages
-  #   package 'zip', :ensure => :installed
-  # 
-  #   cron 'dummy_task', :command => 'ls -la', :user => configuration[:user], :minute => 0, :hour => 0
-  # 
-  #   farm_config = <<-CONFIG
-  #     MOOCOWS = 3
-  #     HORSIES = 10
-  #   CONFIG
-  #   file '/etc/farm.conf', :ensure => :present, :content => farm_config
-  # end
-  # recipe :application_packages
+  
+  recipe :memcached
+  
+  def application_packages
+    package 'zip', :ensure => :installed
+  
+    cron 'dummy_task', :command => 'ls -la', :user => configuration[:user], :minute => 0, :hour => 0
+  
+    farm_config = <<-CONFIG
+      MOOCOWS = 3
+      HORSIES = 10
+    CONFIG
+    file '/etc/farm.conf', :ensure => :present, :content => farm_config
+  end
+  recipe :application_packages
   
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Bare EC2 intance
-server 'ec2-50-17-29-166.compute-1.amazonaws.com', :app, :web, :db, :primary => true
+server 'ec2-50-17-29-166.compute-1.amazonaws.com', :app, :web, :db, :primary 
 
 # After deploy:setup
 # server 'ec2-50-17-24-250.compute-1.amazonaws.com', :app, :web, :db, :primary => true
